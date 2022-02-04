c2817d828ecee5b11e32b07c7287924996cb6d8e
Composite <ukjinplant@msn.com>
2022-02-04 18:07:12 +0900

    icon

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let name = 'circle'
+</script>
+<svg class={$$restProps.class} style={$$restProps.style}>
+  <use href={`https://unpkg.com/feather-icons@4.28.0/dist/feather-sprite.svg#${name}`}/>
+</svg>
+<style>
+  svg {
+    width: 24px;
+    height: 24px;
+    stroke: currentColor;
+    stroke-width: 2;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    fill: none;
+  }
+</style>
