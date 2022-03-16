a41f07f2692a9beed799f4c0327319f795ff9525
Composite <ukjinplant@msn.com>
2022-03-16 15:25:05 +0900

    loading static

--- src/main/Boot.svelte
+++ src/main/Boot.svelte
@@ -1,40 +1,42 @@
+<div class="logo-container"></div>
 <div class="loading-container">
   <div class="loading"></div>
 </div>
+<div class="text-container">&copy; Ukjin "Composite" Yang</div>
+<div class="text-container text-right">comne.kr</div>
 <style>
   :global(#app) {
     display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    align-items: center;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    align-content: flex-end;
   }
 
+  .logo-container {
+    flex: 1 1 100%;
+  }
   .loading-container {
-    flex: 0 1 25%;
+    flex: 0 1 100%;
+    padding-bottom:10vh;
+  }
+  .text-container {
+    flex: 0 1 50%;
+    padding: 2rem;
+    color:white;
   }
 
   .loading {
-
+    margin: 0 auto;
     width: 10rem;
     height: 1rem;
-  }
-
-  .loading:before {
-    display: block;
-    width: 100%;
-    height: 100%;
-    content: ' ';
-    background: linear-gradient(90deg, rgba(0,0,0,0) -1%, rgba(0,255,0,1) -1%, rgba(0,255,0,1) 2%, rgba(0,0,0,0) 2%, rgba(0,0,0,0) 3%, rgba(0,255,0,1) 3%, rgba(0,255,0,1) 6%, rgba(0,0,0,0) 6%);
-  }
-
-  .loading:after {
-    display: block;
-    width: 100%;
-    height: 100%;
-    content: ' ';
-    position: relative;
-    top: 0; bottom: 0; left: 0; right: 0;
-    border-radius: .5rem;
+    border-radius: .25rem;
     border: .1rem solid white;
+    background-image:
+      linear-gradient(180deg, rgba(72, 72, 72, 1) 0%, rgba(72, 72, 72, 1) 10%, rgba(192, 192, 192, 1) 40%, rgba(72, 72, 72, 1) 50%, rgba(72, 72, 72, 1) 100%),
+      linear-gradient(180deg, rgba(72, 72, 72, 1) 0%, rgba(72, 72, 72, 1) 10%, rgba(192, 192, 192, 1) 40%, rgba(72, 72, 72, 1) 70%, rgba(72, 72, 72, 1) 100%),
+      linear-gradient(180deg, rgba(72, 72, 72, 1) 0%, rgba(72, 72, 72, 1) 10%, rgba(192, 192, 192, 1) 40%, rgba(72, 72, 72, 1) 90%, rgba(72, 72, 72, 1) 100%);
+    background-size: 6% 80%, 6% 80%, 6% 80%;
+    background-position: 10% 50%, 18% 50%, 26% 50%;
+    background-repeat: no-repeat,no-repeat,no-repeat;
   }
 </style>
